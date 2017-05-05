66ab55f03a30e74cc6b4a8571d854ab56fe119b4
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-05 15:59:12 -0700

    Added jQuery functionality to add and list items in cart

--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
+let subtotal = 0;
+
 $('.card-action').click(function() {
 
-  // In the HTML, the ADD button is the direct sibling of the of the content, of which the name is the first child and the price is the last (second) child
+  // In the HTML, the ADD button is the direct sibling of the of the item content
 
-  let itemName = $(this).prev().children(":first").text(); // use for summary in cart
-  let itemPriceString = $(this).prev().children(":last").text(); // keep this around for the summary in cart
-  let itemPrice = parseFloat(itemPriceString.substr(1)); // use for calculation
+  // , of which the name is the first child
+  let itemName = $(this).prev().children(":first").text();
 
-  console.log(itemName);
-  console.log(itemPrice);
+  // and the price is the last (second) child
+  let itemPriceString = $(this).prev().children(":last").text();
+  let itemPrice = parseFloat(itemPriceString.substr(1));
 
+  // UPDATE TABLE
+  updateTable(itemName, itemPrice);
 })
+
+function updateTable(name, price) {
+  const table = $('table')
+  const tbody = $('tbody')
+
+  // 1st column: Item
+  let col1 = $('<td>').text(name).addClass("name");
+  // 2nd column: Filler (Quantity stretch goal)
+  let col2 = $('<td>').text("").addClass("quantity")
+  // 3rd column: Price
+  let col3 = $('<td>').text("$" + price).addClass("price");
+
+  // Each tr has a unique id of the name of the item
+  let tr = $('<tr>').append(col1).append(col2).append(col3);
+
+  $(tbody).append(tr);
+
+  $(table).append(tbody);
+
+  // SHOW TOTALS
+  $('tfoot').removeClass('hide');
+
+  // UPDATE SUBTOTAL, TAX, TOTAL
+  subtotal += price;
+  let tax = subtotal * 0.10;
+  let total = subtotal + tax;
+
+  // DISPLAY TOTALS
+  $('#subtotal').text("$" + subtotal);
+  $('#tax').text("$" + tax.toFixed(2));
+  $('#total').text("$" + total.toFixed(2));
+}
+
+
+
+
+
+
+
+
+
+//
